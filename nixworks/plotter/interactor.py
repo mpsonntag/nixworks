ee068b60004d3a50dfe5a81d66a1f136892ac136
Chek Yin Choi <chekchek2007@gmail.com>
2019-07-19 19:07:46 +0200

    Fix Image-type check bug and PEP8 fixes

--- nixworks/plotter/interactor.py
+++ nixworks/plotter/interactor.py
@@ -16,16 +16,15 @@ class Interactor:
 
         self.fig = fig
         self.ax = ax
-        self.plotter_list= []
+        self.plotter_list = []
         # Tag for later references to all plotted objects
         self.mpl_tag = None
         # List for labels
-        self.labels= {'x': None, 'y': None, 'x_unit': None, 'y_unit': None}
+        self.labels = {'x': None, 'y': None, 'x_unit': None, 'y_unit': None}
         # List of line references for later use
         self.arrays = []
         self.check_box = []
         self.mpl_artists = []
-        self.image_bool = np.any(isinstance(p, ImagePlotter) for p in self.plotter_list)
 
     @staticmethod
     def _check_da_combination(data_arrays):
@@ -35,27 +34,28 @@ class Interactor:
         if np.any([len(da.dimensions) > 2 and type(suggested_plotter(da))
                    == ImagePlotter for da in data_arrays]):
             return True
-        #TODO: check scalable when they are si/ but not si is ok
+        # TODO: check scalable when they are si/ but not si is ok
         bd = guess_best_xdim(data_arrays[0])
         if isinstance(data_arrays[0].dimensions[bd], nix.SetDimension):
-            for i, da in enumerate(data_arrays):
+            for i, cda in enumerate(data_arrays):
                 bd = guess_best_xdim(data_arrays[0])
-                if not isinstance(da.dimensions[bd], nix.SetDimension):
+                if not isinstance(cda.dimensions[bd], nix.SetDimension):
                     return False
         else:
             dim_u = data_arrays[0].dimensions[bd].unit
-            for i, da in enumerate(data_arrays):
+            for i, cda in enumerate(data_arrays):
                 bd = guess_best_xdim(data_arrays[0])
-                if isinstance(da.dimensions[bd], nix.SetDimension):
+                if isinstance(cda.dimensions[bd], nix.SetDimension):
                     return False
-                if u and not util.units.scalable(da.unit, u):
+                if u and not util.units.scalable(cda.unit, u):
                     return False
-                if dim_u and not util.units.scalable(da.dimensions[bd].unit, dim_u):
+                if dim_u and not util.units.scalable(cda.dimensions[bd].unit,
+                                                     dim_u):
                     return False
         return True
 
     def _plot_da(self, data_arrays, maxpoints):
-        plotter_list = [suggested_plotter(da) for da in data_arrays]
+        plotter_list = [suggested_plotter(d) for d in data_arrays]
 
         for a in plotter_list:
             if isinstance(a, LinePlotter):
@@ -63,9 +63,10 @@ class Interactor:
             else:
                 a.plot(axis=self.ax)
             self._populate_artist(a)
-
-        if not self.image_bool:
-            xlabel = create_label(plotter_list[0].array.dimensions[plotter_list[0].xdim])
+        if not np.any([isinstance(pl, ImagePlotter)
+                       for pl in plotter_list]):
+            xlabel = create_label(plotter_list[0].array.
+                                  dimensions[plotter_list[0].xdim])
             ylabel = create_label(plotter_list[0].array)
             plt.xlabel(xlabel)
             plt.ylabel(ylabel)
@@ -75,11 +76,13 @@ class Interactor:
         plt.show()
         self.plotter_list = plotter_list
 
-    def interact_da(self, data_arrays, enable_tag=True, enable_xzoom=True, maxpoints=None, enable_yzoom=False):
+    def interact_da(self, data_arrays, enable_tag=True, enable_xzoom=True,
+                    maxpoints=None, enable_yzoom=False):
         # Check if the DataArrays can be plotted together
         if not self._check_da_combination(data_arrays):
             raise ValueError('Cannot plot these DataArrays in the same graph.')
-        # Set maxpoints to the no. points of the longest array to be plotted if not being customized
+        # Set maxpoints to the no. points of the longest array to be plotted
+        # if not being customized
         if maxpoints is None:
             maxpoints = len(max(data_arrays, key=len))
         self.arrays = data_arrays
@@ -87,7 +90,8 @@ class Interactor:
         self._plot_da(data_arrays, maxpoints=maxpoints)
         # Setting up checkboxes for interaction of da_visibility
         da1d_idx = np.arange(len(data_arrays))
-        self.check_box = [widgets.Checkbox(True,description=str(data_arrays[n].name)) for n in da1d_idx]
+        self.check_box = [widgets.Checkbox(True, description=str
+                          (data_arrays[n].name)) for n in da1d_idx]
         for box in self.check_box:
             box.observe(self._da_visibility, names='value')
             display.display(box)
@@ -101,23 +105,27 @@ class Interactor:
                 self.ax.legend().set_visible(True)
                 self.fig.canvas.draw_idle()
 
-        if not self.image_bool:
+        if not np.any([isinstance(pl, ImagePlotter)
+                       for pl in self.plotter_list]):
             legend_box = widgets.Checkbox(True, description='Legends')
             legend_box.observe(legend_visibility, names='value')
             display.display(legend_box)
         # Interactive Tagged Area
-        #TODO: Change tag style for Image plotter
         if enable_tag:
             tag_drop = self._reverse_search_tag(data_arrays)
             interact(self._mark_tag, tag=tag_drop)
         # Interactive Sliders for zooming on x-axis
         if enable_xzoom:
-            x_start = widgets.FloatSlider(self.ax.get_xlim()[0], description='X axis start')
-            x_end = widgets.FloatSlider(self.ax.get_xlim()[1], description='X axis end')
+            x_start = widgets.FloatSlider(self.ax.get_xlim()[0],
+                                          description='X axis start')
+            x_end = widgets.FloatSlider(self.ax.get_xlim()[1],
+                                        description='X axis end')
+
             def change_x_start(start):
                 self.ax.set(xlim=(start['new'], x_end.value))
                 self.fig.canvas.draw_idle()
             x_start.observe(change_x_start, names='value')
+
             def change_x_end(end):
                 self.ax.set(xlim=(x_start.value, end['new']))
                 self.fig.canvas.draw_idle()
@@ -125,12 +133,16 @@ class Interactor:
             display.display(x_start, x_end)
 
         if enable_yzoom:
-            y_start = widgets.FloatSlider(self.ax.get_ylim()[0], description='Y axis start')
-            y_end = widgets.FloatSlider(self.ax.get_ylim()[1], description='Y axis end')
+            y_start = widgets.FloatSlider(self.ax.get_ylim()[0],
+                                          description='Y axis start')
+            y_end = widgets.FloatSlider(self.ax.get_ylim()[1],
+                                        description='Y axis end')
+
             def change_y_start(start):
                 self.ax.set(ylim=(start['new'], y_end.value))
                 self.fig.canvas.draw_idle()
             y_start.observe(change_y_start, names='value')
+
             def change_y_end(end):
                 self.ax.set(ylim=(y_start.value, end['new']))
                 self.fig.canvas.draw_idle()
@@ -163,8 +175,8 @@ class Interactor:
                 self.mpl_tag = None
         else:
             ref = tag.references
-            for i, da in enumerate(self.arrays):
-                if da not in ref:
+            for i, da_tag in enumerate(self.arrays):
+                if da_tag not in ref:
                     try:
                         self.plotter_list[i].sc.set_visible(False)
                     except AttributeError:
@@ -176,10 +188,12 @@ class Interactor:
                 if self.mpl_tag:
                     self.mpl_tag.remove()
                     self.mpl_tag = None
-                if self.image_bool:
-                    tagged = patches.Rectangle((tag.position[1], tag.position[0]),tag.extent[0],
+                if np.any([isinstance(pl, ImagePlotter)
+                          for pl in self.plotter_list]):
+                    tagged = patches.Rectangle((tag.position[1],
+                                               tag.position[0]), tag.extent[0],
                                                tag.extent[1], linewidth=1,
-                                               edgecolor='r',facecolor='none')
+                                               edgecolor='r', facecolor='none')
                     self.ax.add_patch(tagged)
                 else:
                     tagged = plt.axvspan(x1, x1 + tag.extent[0],
@@ -193,13 +207,14 @@ class Interactor:
                 tagged = plt.plot(x1, y1, 'ro')
                 self.mpl_tag = tagged
 
-    def _reverse_search_tag(self, data_arrays):
+    @staticmethod
+    def _reverse_search_tag(data_arrays):
         # Only for data_arrays within the same block
         tag_list = [None]
         blk = data_arrays[0]._parent
-        for da in data_arrays:
+        for ref_da in data_arrays:
             for tag in blk.tags:
-                if da in tag.references and tag not in tag_list:
+                if ref_da in tag.references and tag not in tag_list:
                     tag_list.append(tag)
         return tag_list
 
@@ -210,15 +225,16 @@ class Interactor:
         elif isinstance(plotter, EventPlotter):
             artist.extend(plotter.sc)
         elif isinstance(plotter, CategoryPlotter):
-            for b in plotter.bars:
-                artist.extend(b.patches)
+            for bar in plotter.bars:
+                artist.extend(bar.patches)
         elif isinstance(plotter, ImagePlotter):
             artist.append(plotter.image)
         else:
             raise TypeError("Invalid Plotter")
         self.mpl_artists.append(artist)
 
-    def check_compatible_arrays(self, base_array, candidate_group=None):
+    @staticmethod
+    def check_compatible_arrays(base_array, candidate_group=None):
         base_type = base_array.type
         list_of_compatible = []
         for candidate_da in candidate_group.data_arrays:
@@ -228,11 +244,11 @@ class Interactor:
 
     def group_arrays_by_compatibility(self, region_of_view):
         type_dict = dict()
-        for da in region_of_view.data_arrays:
-            if da.type in type_dict.keys():
+        for com_da in region_of_view.data_arrays:
+            if com_da.type in type_dict.keys():
                 continue
             type_key, array_in_group = \
-                self.check_compatible_arrays(da, region_of_view)
+                self.check_compatible_arrays(com_da, region_of_view)
             type_dict[type_key] = array_in_group
         for (k, v) in type_dict.items():
             print("Type:{} No of Arrays:{}".format(k, len(v)))
