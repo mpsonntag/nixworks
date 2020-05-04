b20d1ad71586bba6efb53c95a0c12273a77f80dd
M. Sonntag <michael.p.sonntag@gmail.com>
2020-05-04 14:42:35 +0200

    [converters/nix2nwb] Basic file cleanup

--- nixworks/converters/nwb/nix2nwb.py
+++ nixworks/converters/nwb/nix2nwb.py
@@ -1,42 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+This script converts a data set recorded with https://github.com/relacs/relacs
+and stored in the NIX data format to the NWB format.
+"""
 
 import argparse
 import sys
-from datetime import datetime, timedelta
 
+from datetime import datetime, timedelta
 from pathlib import Path
 
 import nixio as nix
+import pynwb as nwb
 import quantities as pq
 
-start = datetime.now()
-import pynwb as nwb
-print(f"nwb loading done in: {datetime.now() - start}")
 
 def make_recoding_time(recording):
-    '''extract date+time from metadata'''
+    """
+    Extract date and time from metadata
+
+    :param recording: NIX metadata containing recording timestamp
+    """
     date = recording['Date']
     time = recording['Time']
-    t = datetime.strptime(time,"%H:%M:%S")
-    d = datetime.fromisoformat(date)
-    return d + timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+    fmt_t = datetime.strptime(time, "%H:%M:%S")
+    fmt_d = datetime.fromisoformat(date)
+
+    return fmt_d + timedelta(hours=fmt_t.hour, minutes=fmt_t.minute, seconds=fmt_t.second)
 
 
 def convert_1d_sampled(out, data, metadata):
-    '''convert one-dimensional sampled data'''
-    spikes = data[:]
+    """
+    Convert one-dimensional sampled data
+
+    :param out: NWB file
+    :param data: NIX DataArray
+    :param metadata: NIX Block metadata
+    """
     unit = data.unit
     dim = data.dimensions[0]
-    q = pq.Quantity(dim.sampling_interval, dim.unit)
-    rate = (1 / q.rescale(pq.s)).rescale(pq.Hz)
+    quant = pq.Quantity(dim.sampling_interval, dim.unit)
+    rate = (1 / quant.rescale(pq.s)).rescale(pq.Hz)
     print(f"{data.name}, 1d, sampled data: {rate} Hz", file=sys.stderr)
-    ts = nwb.TimeSeries(name=data.name, data=data[:], unit=unit, rate=float(rate))
-    out.add_acquisition(ts)
+    time_series = nwb.TimeSeries(name=data.name, data=data[:], unit=unit, rate=float(rate))
+    out.add_acquisition(time_series)
 
 
 def main():
-    '''Main entry point'''
+    """
+    Main entry point of the NIX to NWB converter. The conversion is specific to NIX files
+    created with the relacs electrophysiology recording software.
+    """
     parser = argparse.ArgumentParser(description='nwb2nix')
     parser.add_argument('-v', '--version', action='store_true', default=False)
     parser.add_argument('FILE', type=str)
@@ -47,31 +62,31 @@ def main():
         print(f"NWB: {nwb.__version__}")
         sys.exit(0)
 
-    p = Path(args.FILE).resolve()
-    print(f"Loading {p}", file=sys.stderr)
-    nfd = nix.File.open(str(p), nix.FileMode.ReadOnly)
-    for b in nfd.blocks:
-        md = b.metadata
-        recording = md['Recording']
+    nix_file = Path(args.FILE).resolve()
+    print(f"Loading {nix_file}", file=sys.stderr)
+    nfd = nix.File.open(str(nix_file), nix.FileMode.ReadOnly)
+    for block in nfd.blocks:
+        meta = block.metadata
+        recording = meta['Recording']
         name = recording['Name']
-        dt = make_recoding_time(recording)
-        print(dt)
-        # now to nwb
+        timestamp = make_recoding_time(recording)
+        print(timestamp)
 
+        # Convert to NWB format
+        # TODO session description
         out = nwb.NWBFile(identifier=name,
-                          session_description=name,  #TODO
-                          session_start_time=dt)
+                          session_description=name,
+                          session_start_time=timestamp)
 
-        for data in b.data_arrays:
+        for data in block.data_arrays:
             dims = data.dimensions
             if len(dims) == 1 and dims[0].dimension_type == nix.DimensionType.Sample:
-                convert_1d_sampled(out, data, md)
+                convert_1d_sampled(out, data, meta)
 
         print(f'writing {name}.nwb', file=sys.stderr)
-        with nwb.NWBHDF5IO(f'{name}.nwb', 'w') as w:
-            w.write(out)
+        with nwb.NWBHDF5IO(f'{name}.nwb', 'w') as nwb_file:
+            nwb_file.write(out)
         print("all done", file=sys.stderr)
-
 
 
 if __name__ == "__main__":
