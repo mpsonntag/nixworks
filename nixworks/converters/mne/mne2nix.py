00574efcebcd779f7c35894dd5a90789eeb57790
M. Sonntag <michael.p.sonntag@gmail.com>
2020-05-05 09:12:52 +0200

    [converters/mne2nix] Basic PEP8 cleanup

--- nixworks/converters/mne/mne2nix.py
+++ nixworks/converters/mne/mne2nix.py
@@ -58,13 +58,15 @@ and 'meas_id').  The '_raw_extras' are also stored in the NIX file in a
 separate Section with name "Extras".
 
 """
-import sys
 import os
+import sys
+
 from collections.abc import Iterable, Mapping
-import mne
+
 import matplotlib.pyplot as plt
-import numpy as np
+import mne
 import nixio as nix
+import numpy as np
 
 
 DATA_BLOCK_NAME = "EEG Data Block"
@@ -91,45 +93,45 @@ def plot_channel(data_array, index):
 def create_md_tree(section, values, block):
     if values is None:
         return
-    for k, v in values.items():
-        if v is None:
+    for k, val in values.items():
+        if val is None:
             continue
-        if isinstance(v, Iterable):
-            if not len(v):
+        if isinstance(val, Iterable):
+            if not val:
                 continue
-            ndim = np.ndim(v)
+            ndim = np.ndim(val)
             if ndim > 1:
-                da = block.create_data_array(k, "Multidimensional Metadata",
-                                             data=v)
+                data_array = block.create_data_array(k, "Multidimensional Metadata",
+                                                     data=val)
                 for _ in range(ndim):
-                    da.append_set_dimension()
-                prop = section.create_property(k, da.id)
-                prop.type = str(v.__class__)
-                da.metadata = section
+                    data_array.append_set_dimension()
+                prop = section.create_property(k, data_array.id)
+                prop.type = str(val.__class__)
+                data_array.metadata = section
                 continue
             # check element type
-            if isinstance(v, Mapping):
+            if isinstance(val, Mapping):
                 # Create a new Section to hold the metadata found in the
                 # dictionary
-                subsec = section.create_section(k, str(v.__class__))
-                create_md_tree(subsec, v, block)
+                subsec = section.create_section(k, str(val.__class__))
+                create_md_tree(subsec, val, block)
                 continue
-            if isinstance(v[0], Mapping):
+            if isinstance(val[0], Mapping):
                 # Create a new subsection to hold each nested dictionary as
                 # sub-subsections
-                subsec = section.create_section(k, str(v.__class__))
-                for idx, subd in enumerate(v):
+                subsec = section.create_section(k, str(val.__class__))
+                for idx, subd in enumerate(val):
                     subsubsec = subsec.create_section(f"{k}-{idx}",
                                                       str(subd.__class__))
                     create_md_tree(subsubsec, subd, block)
                 continue
 
         try:
-            prop = section.create_property(k, v)
+            prop = section.create_property(k, val)
         except TypeError:
             # inconsistent iterable types: upgrade to floats
-            prop = section.create_property(k, [float(vi) for vi in v])
-        prop.type = str(v.__class__)
+            prop = section.create_property(k, [float(vi) for vi in val])
+        prop.type = str(val.__class__)
 
 
 def write_single_da(mneraw, block):
@@ -137,118 +139,118 @@ def write_single_da(mneraw, block):
     data = mneraw.get_data()
     time = mneraw.times
 
-    nchan = mneraw.info["nchan"]
-    print(f"Found {nchan} channels with {mneraw.n_times} samples per channel")
+    n_chan = mneraw.info["nchan"]
+    print(f"Found {n_chan} channels with {mneraw.n_times} samples per channel")
 
-    da = block.create_data_array("EEG Data", RAW_DATA_TYPE, data=data)
-    block.groups[RAW_DATA_GROUP_NAME].data_arrays.append(da)
-    da.unit = "V"
+    data_array = block.create_data_array("EEG Data", RAW_DATA_TYPE, data=data)
+    block.groups[RAW_DATA_GROUP_NAME].data_arrays.append(data_array)
+    data_array.unit = "V"
 
-    for dimlen in data.shape:
-        if dimlen == nchan:
+    for dim_len in data.shape:
+        if dim_len == n_chan:
             # channel labels: SetDimension
-            da.append_set_dimension(labels=mneraw.ch_names)
-        elif dimlen == mneraw.n_times:
+            data_array.append_set_dimension(labels=mneraw.ch_names)
+        elif dim_len == mneraw.n_times:
             # times: RangeDimension
             # NOTE: EDF always uses seconds
-            da.append_range_dimension(ticks=time, label="time", unit="s")
+            data_array.append_range_dimension(ticks=time, label="time", unit="s")
 
 
 def write_multi_da(mneraw, block):
     data = mneraw.get_data()
     time = mneraw.times
 
-    nchan = mneraw.info["nchan"]
-    channames = mneraw.ch_names
+    n_chan = mneraw.info["nchan"]
+    chan_names = mneraw.ch_names
 
-    print(f"Found {nchan} channels with {mneraw.n_times} samples per channel")
+    print(f"Found {n_chan} channels with {mneraw.n_times} samples per channel")
 
     # find the channel dimension to iterate over it
-    for idx, dimlen in enumerate(data.shape):
-        if dimlen == nchan:
-            chanidx = idx
+    for idx, dim_len in enumerate(data.shape):
+        if dim_len == n_chan:
+            chan_idx = idx
             break
     else:
         raise RuntimeError("Could not find data dimension that matches number "
                            "of channels")
 
-    for idx, chandata in enumerate(np.rollaxis(data, chanidx)):
-        chname = channames[idx]
-        da = block.create_data_array(chname, RAW_DATA_TYPE, data=chandata)
-        block.groups[RAW_DATA_GROUP_NAME].data_arrays.append(da)
-        da.unit = "V"
+    for idx, chan_data in enumerate(np.rollaxis(data, chan_idx)):
+        ch_name = chan_names[idx]
+        data_array = block.create_data_array(ch_name, RAW_DATA_TYPE, data=chan_data)
+        block.groups[RAW_DATA_GROUP_NAME].data_arrays.append(data_array)
+        data_array.unit = "V"
 
         # times: RangeDimension
         # NOTE: EDF always uses seconds
-        da.append_range_dimension(ticks=time, label="time", unit="s")
+        data_array.append_range_dimension(ticks=time, label="time", unit="s")
 
 
 def separate_stimulus_types(stimuli):
     # separate stimuli based on label
-    stimdict = dict()
+    stim_dict = dict()
     for label, onset, duration in zip(stimuli.description,
                                       stimuli.onset,
                                       stimuli.duration):
-        if label not in stimdict:
-            stimdict[label] = [(label, onset, duration)]
+        if label not in stim_dict:
+            stim_dict[label] = [(label, onset, duration)]
         else:
-            stimdict[label].append((label, onset, duration))
-    return stimdict
+            stim_dict[label].append((label, onset, duration))
+    return stim_dict
 
 
 def write_stim_tags(mneraw, block, split):
     stimuli = mneraw.annotations
 
     if split:
-        stimtuples = separate_stimulus_types(stimuli)
-        for label, st in stimtuples.items():
+        stim_tuples = separate_stimulus_types(stimuli)
+        for label, stim in stim_tuples.items():
             label = label.replace("/", "|")
-            create_stimulus_multi_tag(st, block, mneraw, mtagname=label)
+            create_stimulus_multi_tag(stim, block, mneraw, mtagname=label)
     else:
-        stimtuples = [(l, o, d) for l, o, d in zip(stimuli.description,
-                                                   stimuli.onset,
-                                                   stimuli.duration)]
-        create_stimulus_multi_tag(stimtuples, block, mneraw)
+        stim_tuples = [(desc, onset, dur) for desc, onset, dur in zip(stimuli.description,
+                                                                      stimuli.onset,
+                                                                      stimuli.duration)]
+        create_stimulus_multi_tag(stim_tuples, block, mneraw)
 
 
 def create_stimulus_multi_tag(stimtuples, block, mneraw, mtagname="Stimuli"):
     # check dimensionality of data
-    datashape = block.groups[RAW_DATA_GROUP_NAME].data_arrays[0].shape
+    data_shape = block.groups[RAW_DATA_GROUP_NAME].data_arrays[0].shape
 
     labels = [st[0] for st in stimtuples]
     onsets = [st[1] for st in stimtuples]
     durations = [st[2] for st in stimtuples]
 
-    ndim = len(datashape)
-    if ndim == 1:
+    n_dim = len(data_shape)
+    if n_dim == 1:
         positions = onsets
         extents = durations
     else:
-        channelextent = mneraw.info["nchan"] - 1
-        positions = [(0, p) for p in onsets]
-        extents = [(channelextent, e) for e in durations]
+        channel_extent = mneraw.info["nchan"] - 1
+        positions = [(0, pos) for pos in onsets]
+        extents = [(channel_extent, ext) for ext in durations]
 
-    posda = block.create_data_array(f"{mtagname} onset", "Stimuli Positions",
-                                    data=positions)
-    posda.append_set_dimension(labels=labels)
+    pos_da = block.create_data_array(f"{mtagname} onset", "Stimuli Positions",
+                                     data=positions)
+    pos_da.append_set_dimension(labels=labels)
 
-    extda = block.create_data_array(f"{mtagname} durations", "Stimuli Extents",
-                                    data=extents)
-    extda.append_set_dimension(labels=labels)
+    ext_da = block.create_data_array(f"{mtagname} durations", "Stimuli Extents",
+                                     data=extents)
+    ext_da.append_set_dimension(labels=labels)
 
-    for _ in range(ndim-1):
+    for _ in range(n_dim-1):
         # extra set dimensions for any extra data dimensions (beyond the first)
-        posda.append_set_dimension()
-        extda.append_set_dimension()
+        pos_da.append_set_dimension()
+        ext_da.append_set_dimension()
 
-    stimmtag = block.create_multi_tag(mtagname, "EEG Stimuli",
-                                      positions=posda)
-    stimmtag.extents = extda
-    block.groups[RAW_DATA_GROUP_NAME].multi_tags.append(stimmtag)
+    stim_mtag = block.create_multi_tag(mtagname, "EEG Stimuli",
+                                       positions=pos_da)
+    stim_mtag.extents = ext_da
+    block.groups[RAW_DATA_GROUP_NAME].multi_tags.append(stim_mtag)
 
-    for da in block.groups[RAW_DATA_GROUP_NAME].data_arrays:
-        if da.type == RAW_DATA_TYPE:
-            stimmtag.references.append(da)
+    for data_array in block.groups[RAW_DATA_GROUP_NAME].data_arrays:
+        if data_array.type == RAW_DATA_TYPE:
+            stim_mtag.references.append(data_array)
 
 
 def write_raw_mne(nfname, mneraw,
@@ -265,15 +267,15 @@ def write_raw_mne(nfname, mneraw,
     MultiTags based on the stimulus type (label).
     :rtype: None
     """
-    mneinfo = mneraw.info
-    extrainfo = mneraw._raw_extras
+    mne_info = mneraw.info
+    extra_info = mneraw._raw_extras
 
     # Create NIX file
-    nf = nix.File(nfname, nix.FileMode.Overwrite)
+    nix_file = nix.File(nfname, nix.FileMode.Overwrite)
 
     # Write Data to NIX
-    block = nf.create_block(DATA_BLOCK_NAME, DATA_BLOCK_TYPE,
-                            compression=nix.Compression.DeflateNormal)
+    block = nix_file.create_block(DATA_BLOCK_NAME, DATA_BLOCK_TYPE,
+                                  compression=nix.Compression.DeflateNormal)
     block.create_group(RAW_DATA_GROUP_NAME, RAW_DATA_GROUP_TYPE)
 
     if split_data_channels:
@@ -286,20 +288,20 @@ def write_raw_mne(nfname, mneraw,
 
     # Write metadata to NIX
     # info dictionary
-    infomd = nf.create_section("Info", "File metadata")
-    create_md_tree(infomd, mneinfo, block)
+    info_md = nix_file.create_section("Info", "File metadata")
+    create_md_tree(info_md, mne_info, block)
     # extras
-    if len(extrainfo) > 1:
-        for idx, emd_i in enumerate(extrainfo):
-            extrasmd = nf.create_section(f"Extras-{idx}",
-                                         "Raw Extras metadata")
-            create_md_tree(extrasmd, emd_i, block)
-    elif extrainfo:
-        extrasmd = nf.create_section("Extras", "Raw Extras metadata")
-        create_md_tree(extrasmd, extrainfo[0], block)
+    if len(extra_info) > 1:
+        for idx, emd_i in enumerate(extra_info):
+            extras_md = nix_file.create_section(f"Extras-{idx}",
+                                                "Raw Extras metadata")
+            create_md_tree(extras_md, emd_i, block)
+    elif extra_info:
+        extras_md = nix_file.create_section("Extras", "Raw Extras metadata")
+        create_md_tree(extras_md, extra_info[0], block)
 
     # all done
-    nf.close()
+    nix_file.close()
     print(f"Created NIX file at '{nfname}'")
     print("Done")
 
@@ -311,38 +313,38 @@ def main():
               "an EDF filename as the first argument")
         sys.exit(1)
 
-    splitdata = False
+    split_data = False
     if "--split-data" in args:
-        splitdata = True
+        split_data = True
         args.remove("--split-data")
 
-    splitstim = False
+    split_stim = False
     if "--split-stimuli" in args:
-        splitstim = True
+        split_stim = True
         args.remove("--split-stimuli")
 
-    datafilename = args[1]
+    data_filename = args[1]
     montage = None
     if len(args) > 2:
         montage = args[2]
         montage = os.path.abspath(montage)
-    root, ext = os.path.splitext(datafilename)
-    nfname = root + os.path.extsep + "nix"
+    root, ext = os.path.splitext(data_filename)
+    nf_name = root + os.path.extsep + "nix"
     if ext.casefold() == ".edf".casefold():
-        mneraw = mne.io.read_raw_edf(datafilename, montage=montage,
+        mneraw = mne.io.read_raw_edf(data_filename, montage=montage,
                                      preload=True, stim_channel=False)
     elif ext.casefold() == ".vhdr".casefold():
-        mneraw = mne.io.read_raw_brainvision(datafilename, montage=montage,
+        mneraw = mne.io.read_raw_brainvision(data_filename, montage=montage,
                                              preload=True, stim_channel=False)
     else:
         raise RuntimeError(f"Unknown extension '{ext}'")
-    print(f"Converting '{datafilename}' to NIX")
-    if splitdata:
+    print(f"Converting '{data_filename}' to NIX")
+    if split_data:
         print("  Creating one DataArray per channel")
-    if splitstim:
+    if split_stim:
         print("  Creating one MultiTag for each stimulus type")
 
-    write_raw_mne(nfname, mneraw, splitdata, splitstim)
+    write_raw_mne(nf_name, mneraw, split_data, split_stim)
 
     mneraw.close()
 
